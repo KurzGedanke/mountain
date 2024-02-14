f19761c7ac28db29b3c70b2274ddb8456918005d
Thore Jahn <tj@move-elevator.de>
2024-02-14 15:56:11 +0100

    Initial Commit

---
+++ mountain/mountainApp.swift
@@ -0,0 +1,17 @@
+//
+//  mountainApp.swift
+//  mountain
+//
+//  Created by moveguard on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct mountainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

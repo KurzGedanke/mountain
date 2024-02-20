5519be466b1dee104e5eaff82fc5fa8454e36a75
KurzGedanke <git@kurzgedanke.me>
2024-02-20 23:06:30 +0100

    added things to timer

--- mountain/TimerView.swift
+++ mountain/TimerView.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import Foundation
+
+let fmt = ISO8601DateFormatter()
+
+let date1 = Date.now
+let date2 = fmt.date(from: "2024-07-11T12:00:00+0100")!
+
+let diffs = Calendar.current.dateComponents([.second], from: date1, to: date2).second
 
 struct TimerView: View {
-    @State var timeRemaining = 10000
+    @State var timeRemaining = diffs!
     @State private var currentDate = Date.now
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
